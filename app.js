551e7e6514ca9fab3434e04dc7094b71f3e5d15c
jangjunhyeok <choigod10234@gmail.com>
2018-10-02 22:23:24 +0900

    digitalcon

--- app.js
+++ app.js
@@ -43,23 +43,17 @@ app.use(passport.session());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-var asdf;
 let d;
 var salt = 'jjang486';
 
 passport.use(new LocalStrategy({
-        usernameField: 'username',
-        passwordField: 'password',
-        passReqToCallback: true //인증을 수행하는 인증 함수로 HTTP request를 그대로  전달할지 여부를 결정한다
-    },
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true //인증을 수행하는 인증 함수로 HTTP request를 그대로  전달할지 여부를 결정한다
+},
     function (req, username, password, done) {
         var data;
         var query = conn.query(`SELECT * from test WHERE accessid =\"${username}\"`, function (err, rows) {
-            console.log(rows[0].accessid)
-            console.log(username);
-            console.log(rows[0].password)
-            console.log(sha256(salt + password))
-            asdf = rows[0].NAME;
             if (username === rows[0].accessid && sha256(salt + password) === rows[0].password) {
 
                 return done(null, {
@@ -74,41 +68,64 @@ passport.use(new LocalStrategy({
 passport.use(new FacebookStrategy({
     clientID: '305245486944906',
     clientSecret: '8085e26f799a616db568eecd070428bc',
-    callbackURL:"http://localhost:3000/auth/facebook/callback"
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
 },
-function(accessToken,refreshToken,profile,done){
-    
-    name = profile.displayName;
-    console.log(name);
-    asdf = name;
-    if(name == "장준혁"){
-        return done(null,{
-            'user_id':asdf,
-        });
-    }else{
-        return done(false,null);
-    }
-}))
+    function (accessToken, refreshToken, profile, done) {
+        userid = profile.id;
+        name = profile.displayName;
+
+        var query = conn.query(`SELECT * from facebook WHERE ID=\"${userid}\"`, function (err, data, fields) {
+            if (err) throw err;
+            d = data.length;
+            var users = {
+                'ID': userid,
+                'NAME': name
+            }
+            if (!d) {
+                var query = conn.query('insert into facebook set ?', users, function (err, data, field) {
+                    if (err) throw err;
+                    return done(null, {
+                        'user_id': name
+                    })
+                })
+            } else {
+                return done(null, {
+                    'user_id': name
+                })
+            }
+        })
+    }))
 
 var name;
 passport.use('kakao-login', new KakaoStrategy({
-        clientID: '79811515dddf1938551b4588b0f04f76',
-        clientSecret: 'ruXMRDw3iLxTmJjomhI6EHVBBZuFe1rv',
-        callbackURL: 'http://localhost:3000/oauth/kakao/callback'
-    },
+    clientID: '79811515dddf1938551b4588b0f04f76',
+    clientSecret: 'ruXMRDw3iLxTmJjomhI6EHVBBZuFe1rv',
+    callbackURL: 'http://localhost:3000/oauth/kakao/callback'
+},
     function (accessToken, refreshToken, profile, done) {
-        // console.log(profile);
-        console.log(profile);
-        name = profile.username;
-        asdf = name;
-        console.log(name);
-        if (name === "장준혁") {
-            return done(null, {
-                'user_id': asdf,
-            });
-        } else {
-            return done(false, null);
-        }
+        userid = profile.id;
+        name = profile.displayName;
+
+        var query = conn.query(`SELECT * from kakao WHERE ID=\"${userid}\"`, function (err, data, fields) {
+            if (err) throw err;
+            d = data.length;
+            var users = {
+                'ID': userid,
+                'NAME': name
+            }
+            if (!d) {
+                var query = conn.query('insert into kakao set ?', users, function (err, data, field) {
+                    if (err) throw err;
+                    return done(null, {
+                        'user_id': name
+                    })
+                })
+            } else {
+                return done(null, {
+                    'user_id': name
+                })
+            }
+        })
     }
 ))
 
@@ -122,15 +139,16 @@ passport.deserializeUser(function (user, done) {
 
 app.use(express.static('public'))
 
+
 app.get('/kakao', passport.authenticate('kakao-login'));
 
 app.get('/login', function (req, res) {
     res.sendfile('./public/login.html');
 })
 app.post('/login', passport.authenticate('local', {
-        failureRedirect: '/login',
-        failureFlash: true
-    }), // 인증 실패 시 401 리턴, {} -> 인증 스트레티지
+    failureRedirect: '/login',
+    failureFlash: true
+}), // 인증 실패 시 401 리턴, {} -> 인증 스트레티지
     function (req, res) {
         res.redirect('/myinfo');
     });
@@ -139,15 +157,15 @@ app.get('/register', function (req, res) {
     res.sendfile('./public/register.html');
 })
 
-app.get('/facebook',passport.authenticate('facebook'));
+app.get('/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback',passport.authenticate('facebook',{
-    failureRedirect : '/login',
-    failureFlash : true
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    failureFlash: true
 }),
-function(req,res){
-    res.redirect('/myinfo');
-})
+    function (req, res) {
+        res.redirect('/myinfo');
+    })
 
 var id = 2;
 
@@ -175,33 +193,38 @@ app.post('/register', function (req, res) {
                 res.redirect('/login');
             })
         } else {
-            res.render('read',{title:'회원가입 실패',pass:true})
+            res.render('read', { title: '회원가입 실패', pass: true })
         }
-        console.log(d);
     })
 
 })
 
 app.get('/oauth/kakao/callback', passport.authenticate('kakao-login', {
-        failureRedirect: '/login',
-        failureFlash: true
-    }),
+    failureRedirect: '/login',
+    failureFlash: true
+}),
     function (req, res) {
         res.redirect('/myinfo');
     })
 
 app.get('/myinfo', isAuthenticated, function (req, res) {
-    if (asdf === undefined)
+    if (name === undefined)
         return res.redirect('/login')
     res.render('myinfo', {
         title: 'My Info',
-        user_info: asdf
+        user_info: name
     })
 });
 
+app.get('/',function(req,res){
+    res.render('main',{
+        title : 'Sunrin Life',
+        user_info:name
+    })
+})
 app.get('/logout', function (req, res) {
     req.logout();
-    res.redirect('/');
+    res.redirect('/login');
 });
 
 app.listen(3000, function () {
